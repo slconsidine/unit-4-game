d9847caed37f9bdc2f08f13322a496d6cd2469fc
Sam <samlconsidine@gmail.com>
2020-01-18 15:30:24 -0500

    Each button is working to show the user they won or lost and the counters for each are updating. Random number is regenerated after each win or loss.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
         console.log(random); 
         $("#random").text(random);
 
-
     // Generates random number for crystals
     var crystal1 = Math.floor(Math.random()*12 + 1);
     var crystal2 = Math.floor(Math.random()*12) + 1;
@@ -22,26 +21,94 @@ $(document).ready(function() {
     // Click a crystal and logging the total in the count
     var myCounter = 0;
         $("#userScore").text(myCounter);
+    var win = 0;
+        $("#wins").text(win);
+    var loss = 0;
+        $("#losses").text(loss);
+        console.log(win);
 
+    
     $(".crystal1").click(function() {
         myCounter+=crystal1;
         $("#userScore").text(myCounter);
         if (myCounter == random) {
             alert("winner");
+            win++;
+            $("#wins").text(win);
+            random = Math.floor(Math.random() * 102 + 19);
+            $("#random").text(random);  
+            myCounter = 0;
+            $("#userScore").text(myCounter);
+                crystal1 = Math.floor(Math.random()*12 + 1);
+                $("#crystal1").text(crystal1);
+                console.log(crystal1)
+        } else if (myCounter > random) {
+            alert("loser");
+            loss++;
+            $("#losses").text(loss);
+            random = Math.floor(Math.random() * 102 + 19);
+            $("#random").text(random);  
+            myCounter = 0;
+            $("#userScore").text(myCounter);
+            crystal1 = Math.floor(Math.random()*12 + 1);
+            $("#crystal1").text(crystal1);
+            console.log(crystal1);
         }
     });
+
     $(".crystal2").click(function() {
         myCounter+=crystal2;
         $("#userScore").text(myCounter);
         if (myCounter == random) {
             alert("winner");
-        }
+            win++;
+            $("#wins").text(win);
+            random = Math.floor(Math.random() * 102 + 19);
+            $("#random").text(random);
+            myCounter = 0;
+            $("#userScore").text(myCounter);
+            crystal2 = Math.floor(Math.random()*12 + 1);
+            $("#crystal2").text(crystal2); 
+            console.log(crystal2);
+        } else if (myCounter > random) {
+            alert("loser");
+            loss++;
+            $("#losses").text(loss);
+            random = Math.floor(Math.random() * 102 + 19);
+            $("#random").text(random);  
+            myCounter = 0;
+            $("#userScore").text(myCounter);
+            crystal2 = Math.floor(Math.random()*12 + 1);
+            $("#crystal2").text(crystal2);
+            console.log(crystal2);
+        }             
     });
     $(".crystal3").click(function() {
         myCounter+=crystal3;
         $("#userScore").text(myCounter);
         if (myCounter == random) {
             alert("winner");
+            win++;
+            $("#wins").text(win);
+            random = Math.floor(Math.random() * 102 + 19);
+            $("#random").text(random);
+            myCounter = 0;
+            $("#userScore").text(myCounter);
+            crystal3 = Math.floor(Math.random()*12 + 1);
+            $("#crystal3").text(crystal3);
+            console.log(crystal3);
+        }
+        else if (myCounter > random) {
+            alert("loser");
+            loss++;
+            $("#losses").text(loss);
+            random = Math.floor(Math.random() * 102 + 19);
+            $("#random").text(random);  
+            myCounter = 0;
+            $("#userScore").text(myCounter);
+            crystal3 = Math.floor(Math.random()*12 + 1);
+            $("#crystal3").text(crystal3);  
+            console.log(crystal3);
         }
     });
     $(".crystal4").click(function() {
@@ -49,7 +116,28 @@ $(document).ready(function() {
         $("#userScore").text(myCounter);
         if (myCounter == random) {
             alert("winner");
+            win++;
+            $("#wins").text(win);
+            random = Math.floor(Math.random() * 102 + 19);
+            $("#random").text(random);
+            myCounter = 0;
+            $("#userScore").text(myCounter);
+            crystal4 = Math.floor(Math.random()*12 + 1);
+            $("#crystal4").text(crystal4);
+            console.log(crystal4);
         }
+        else if (myCounter > random) {
+            alert("loser");
+            loss++;
+            $("#losses").text(loss);
+            random = Math.floor(Math.random() * 102 + 19);
+            $("#random").text(random);  
+            myCounter = 0;
+            $("#userScore").text(myCounter);
+            crystal4 = Math.floor(Math.random()*12 + 1);
+            $("#crystal4").text(crystal4);
+            console.log(crystal4);
+        }             
     });
 
 });
