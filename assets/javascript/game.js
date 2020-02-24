5493c10e15517a2be2cef0ad6a107aef93d66b98
Sam <samlconsidine@gmail.com>
2020-02-24 10:27:35 -0500

    Changed theme of game and added new styling.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     var crystal4;
     var setCrystals = function() {
         crystal1 = Math.floor(Math.random()*12 + 1);
-        crystal2 = Math.floor(Math.random()*12) + 1;
+        crystal2 = Math.floor(Math.random()*12 + 1);
         crystal3 = Math.floor(Math.random()*12 + 1);
         crystal4 = Math.floor(Math.random()*12 + 1);
             $("#crystal1").text(crystal1);
@@ -103,17 +103,3 @@ $(document).ready(function() {
     });
 
 });
-
-
-//PSEUDOCODE
-// 1. generate random number to guess - done
-//      1a. put this in random number on HTML - done
-// 2. assign a random value between 1-12 to each crystal - done
-//      2a. put this over each value of crystals - done
-// 3. grab user's event click for each crystal - done
-// 4. add user's clicks together - done
-// 5. if usertotal = random number you win!
-// 6. if usertotal < random number, keep playing
-// 7. if usertotal > random number, you lose
-// 8. count wins and losses
-// 9. reset everything
